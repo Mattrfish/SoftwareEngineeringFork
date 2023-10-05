fee0dbdd91714b21ac5312687779b299e40b7681
Matthew Fish <146200948+Mattrfish@users.noreply.github.com>
2023-10-05 15:46:22 +0100

    02 - Iteration

--- Tasks/02 - Iteration/211-nested-loops/211-nested-loops.cpp
+++ Tasks/02 - Iteration/211-nested-loops/211-nested-loops.cpp
@@ -10,9 +10,14 @@ int main()
         // Inner loop
         for (int j = 1; j <= 5; j++)    // Inner loop executes 5 times for each outer loop run so 10 time in total
         {
-            printf("\tInner loop number %d\n", j);  // note the use of the tab (\t) option at the start to improve the layout
+            printf("\tInner loop number %d\n", j);
+            printf("\t\ti,j,k\n");
+            printf("\t\t-----\n");// note the use of the tab (\t) option at the start to improve the layout
             
-            //Task - Write another level of nesting here
+            for (int k = 1; k <= 3; k++)
+            {  
+                printf("\t\t%d,%d,%d\n", i, j, k);
+            }
 
         }
     }
